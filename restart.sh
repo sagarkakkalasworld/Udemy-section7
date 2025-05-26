40aa5df8284c0986f28dbeb3537e5e0773d77bda
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-05-26 17:32:20 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-microk8s kubectl rollout restart deployment react-deployment -n react-microk8s
+ssh ubuntu@17.12.34.14 "microk8s kubectl rollout restart deployment react-deployment -n react-microk8s" ##update the private IP
+
