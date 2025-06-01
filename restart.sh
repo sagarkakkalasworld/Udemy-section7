9241752002e145ec9dc5d3dadd33227ee83a12d2
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-06-01 16:23:43 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh ubuntu@172.31.31.45 "microk8s kubectl rollout restart deployment react-deployment -n react-microk8s" ##update the private IP
+ssh ubuntu@172.31.31.45 "microk8s kubectl rollout restart deployment react-deployment -n react-microk8s" ##update the AWS private IP
 
